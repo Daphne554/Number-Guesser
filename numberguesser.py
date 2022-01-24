1a37c1edcec2a587ee7501b8dd2386b8d0260326
Daphne554 <daphnekodua@gmail.com>
2022-01-24 13:00:42 +0000

    Number guesser completed

--- numberguesser.py
+++ numberguesser.py
@@ -14,3 +14,21 @@ else:
     quit()
 
 random_number = random.randint(0, range_limit)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Type in your guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Type a number next time. ")
+        continue
+    if user_guess == random_number:
+        print("You guessed right!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number")
+    elif user_guess < random_number:
+        print("You were below the number")
+
+print(f"You got it in {str(guesses)} guess(es)!!")
