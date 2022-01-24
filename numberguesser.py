46c274bdf5286e3a81fe4839d99e7331d15babd7
Daphne554 <daphnekodua@gmail.com>
2022-01-24 11:27:26 +0000

    Initial commit

---
+++ numberguesser.py
@@ -0,0 +1,14 @@
+import random
+
+range_limit = input("Type a limit for number range: ")
+
+if range_limit.isdigit():
+    range_limit = int(range_limit)
+
+elif range_limit <= 0:
+    print("Type a number larger than zero")
+    quit()
+
+else:
+    print("Type a number next time!")
+    quit()
